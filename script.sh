aca6a6d459bf61fa20cc331762ef904a1052b906
ketan <ketan.chaudhari@zerodha.com>
2025-07-22 03:28:45 +0530

    Add flake.lock and update script.sh for agenix integration

---
+++ script.sh
@@ -0,0 +1,13 @@
+
+# yescrypt your password
+mkpasswd --method=yescrypt
+
+# run agenix 
+cd minibox/secrets
+sudo nix run github:ryantm/agenix -- -e secret1.age
+
+sudo nix run nixpkgs#nixos-anywhere -- \
+        --flake ./minibox#minibox \
+        --generate-hardware-config nixos-generate-config ./minibox/hardware-configuration.nix \
+        --extra-files ./minibox/extra-files \
+        nixos@192.168.29.144
